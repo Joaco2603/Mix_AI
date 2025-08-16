68c9b0008fb3e171140f4b113cc0fd34a4f44f04
joaco@gmail.com <jpappa2603@gmail.com>
2025-08-15 21:24:25 -0600

    add dictionary to keywords, and readmes

--- volume-mcp/src/main/java/com/cenfotec/volumemcp/services/RestTemplateService.java
+++ volume-mcp/src/main/java/com/cenfotec/volumemcp/services/RestTemplateService.java
@@ -10,25 +10,26 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+
 @Service
 public class RestTemplateService {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    private final String baserUrl = "http://194.168.0.4/";
+    private final String baserUrl = "http://192.168.0.4/";
 
     public ResponseEntity<String> setVolume(Integer value, Integer channel) {
         String url = baserUrl + "volume";
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("value", value);
-        body.add("channel", channel);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("value", value.toString());
+        body.add("channel", channel.toString());
 
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
 
         return restTemplate.postForEntity(url, requestEntity, String.class);
     }
